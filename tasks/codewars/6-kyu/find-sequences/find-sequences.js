930a35479b9ac158f8d2bccf430f9bb776440a25
antelopest <antelopest@gmail.com>
2023-08-27 12:47:07 +0300

    main add task

---
+++ tasks/codewars/6-kyu/find-sequences/find-sequences.js
@@ -0,0 +1,12 @@
+/*
+Complete function findSequences. It accepts a positive integer n. Your task is to find such integer sequences:
+
+For example, n = 15
+valid integer sequences:
+[1,2,3,4,5]  (1+2+3+4+5==15)
+[4,5,6]          (4+5+6==15)
+[7,8]              (7+8==15)
+*/
+const findSequences = n => {
+
+}
