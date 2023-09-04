148a14e123892e774592c40e863633ea429ad16b
antelopest <antelopest@gmail.com>
2023-09-04 21:12:30 +0300

    main add task

--- tasks/codewars/8-kyu/smallest-integer-finder/smallst-integer-finder.js
+++ tasks/codewars/8-kyu/smallest-integer-finder/smallst-integer-finder.js
@@ -1,9 +1,9 @@
 /*
-Given an array of integers your solution should find the smallest integer.
+Given an array of integers your short-long-short should find the smallest integer.
 
 For example:
-Given [34, 15, 88, 2] your solution will return 2
-Given [34, -345, -1, 100] your solution will return -345
+Given [34, 15, 88, 2] your short-long-short will return 2
+Given [34, -345, -1, 100] your short-long-short will return -345
 
 spread
 arr = [1,2,3)
