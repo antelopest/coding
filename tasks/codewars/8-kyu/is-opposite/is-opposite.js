d8da46383a058e3c55b731ac83c3a0ed9568a65e
antelopest <antelopest@gmail.com>
2023-09-17 12:18:54 +0300

    main add task

--- tasks/codewars/8-kyu/is-opposite/is-opposite.js
+++ tasks/codewars/8-kyu/is-opposite/is-opposite.js
@@ -10,3 +10,27 @@ Examples (input -> output)
 "AB","Ab"     -> false
 "",""         -> false
 */
+const isOpposite = (s1, s2) => {
+    if (s1.length === 0 && s2.length === 0) return true;
+    if (s1.length === s2.length) {
+        return !s1.split('').filter(
+            (e, i) => {
+                return e.toUpperCase() === s2[i].toUpperCase() &&
+                    e === s2[i];
+            }
+        ).length;
+    } else {
+        return false;
+    }
+}
+
+console.log(
+    isOpposite(
+        "ab", "AB"
+    )
+);
+console.log(
+    isOpposite(
+        "AB", "AB"
+    )
+);
